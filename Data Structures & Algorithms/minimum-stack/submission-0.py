1628dbc8ddaf0e623cecb0a850283aa263181324
KUNAL-CHAUDHARI-KC <kkckunal00@gmail.com>
2026-09-17 15:27:45 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,25 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = [] 
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if self.minStack:
+            minVal = min(self.minStack[-1], val)
+        else:
+            minVal = val
+
+        self.minStack.append(minVal)
+        
+    def pop(self) -> None:
+        self.minStack.pop()
+        return self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+    def getMin(self) -> int:
+        return self.minStack[-1]
+        
